a05107f6d22eef3d96277a0835392296669e574a
L155a <nao_kavakita@mail.ru>
2021-05-29 13:58:15 +0300

    finish2

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -1,10 +1,22 @@
 package subtask2
 
+@Suppress("UNREACHABLE_CODE")
 class BillCounter {
 
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+
+        val fairlySplit = "bon appetit"
+        val check = bill.sum()
+
+        val actual: Int = (check - bill[k]) / 2 // сумма без еды Анны
+
+        val anna = if (actual == b) {
+            fairlySplit
+        } else {
+            (b - actual).toString()
+        }
+        return anna
     }
 }
